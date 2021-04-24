eb3f555c7133fb04b2418cc95612e679dfac9697
Gilles Avraam <gillouavraam@gmail.com>
2021-04-24 12:41:40 +0200

    [add] first commit
    basic set up  of a blog

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles')
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+    const articles = [{
+        title: "test Article",
+        createdAt: new Date(),
+        description: "azertyuiop"
+    },
+        {
+            title: "test Article v2",
+            createdAt: new Date(),
+            description: "azertyuiop"
+        }
+    ]
+    res.render('articles/index', { articles: articles });
+})
+
+app.use('/articles', articleRouter);
+
+app.listen(5000);
